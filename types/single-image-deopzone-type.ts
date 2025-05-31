21d093a597ce81c0d799a640671382345ddd4f35
arsimsejdiu <arsim.sejdiu@bik-air.com>
2025-05-31 12:40:30 +0200

    fixing

--- types/single-image-deopzone-type.ts
+++ types/single-image-deopzone-type.ts
@@ -1,11 +1,11 @@
-import { type DropzoneOptions } from 'react-dropzone';
+import { type DropzoneOptions } from "react-dropzone";
 
 export type InputProps = {
-    width?: number;
-    height?: number;
-    className?: string;
-    value?: File | string;
-    onChange?: (file?: File) => void | Promise<void>;
-    disabled?: boolean;
-    dropzoneOptions?: Omit<DropzoneOptions, 'disabled'>;
-  };
+  width?: number;
+  height?: number;
+  className?: string;
+  value?: File | string;
+  onChange?: (file?: File) => void | Promise<void>;
+  disabled?: boolean;
+  dropzoneOptions?: Omit<DropzoneOptions, "disabled">;
+};
